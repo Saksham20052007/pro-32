346f684827dfd60e180095129e1743babcd73aaa
Saksham20052007 <74138256+Saksham20052007@users.noreply.github.com>
2021-03-20 23:33:30 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,43 @@
+class box{
+    
+    constructor(x, y, width, height){
+
+        var options={
+            'density' : 1.2,
+            'friction' : 0.8,
+            'restitution' :0.5, 
+            
+        }
+
+        this.visiblity = 255;
+        this.width = width;
+        this.height = height;
+    
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        World.add(world, this.body);
+    }
+
+    display(){
+        if(this.body.speed<5){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+        }else{
+            
+            World.remove(world, this.body);
+            push();
+            this.visiblity-=105;
+            pop();
+        }
+        
+    }
+
+
+score(){
+    if(this.visiblity<0 && this.visiblity>-105){
+        score++;
+    }
+}
+}
